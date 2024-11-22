4b275128f2d8b392faea04f55fb0280138b421da
wutongyuonce <3304984553@qq.com>
2024-11-22 17:39:08 +0800

    1

--- homework/homework/homework/homework.cpp
+++ homework/homework/homework/homework.cpp
@@ -1,25 +1,20 @@
 ﻿#include <stdio.h>
 
 int main() {
-	int count = 0; // 计数器，用于每行输出5个闰年
+	// 数字1、2、3、4
+	int digits[] = { 1, 2, 3, 4 };
 
-	// 遍历1900到2018年之间的所有年份
-	for (int year = 1900; year <= 2018; year++) {
-		// 判断是否是闰年
-		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-			printf("%d ", year); // 输出闰年
-			count++; // 计数器增加
-
-			// 如果输出了5个闰年，换行
-			if (count % 5 == 0) {
-				printf("\n");
+	// 使用三重循环生成所有三位数
+	for (int i = 0; i < 4; i++) { // 百位
+		for (int j = 0; j < 4; j++) { // 十位
+			for (int k = 0; k < 4; k++) { // 个位
+				// 保证百位、十位、个位的数字不重复
+				if (i != j && j != k && i != k) {
+					// 输出符合条件的三位数
+					printf("%d%d%d\n", digits[i], digits[j], digits[k]);
+				}
 			}
 		}
-	}
-
-	// 如果最后一行没有满5个闰年，打印换行
-	if (count % 5 != 0) {
-		printf("\n");
 	}
 
 	return 0;
