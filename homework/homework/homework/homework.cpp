19eda1ec26fdf86842ff4bd6d28f405444566ec8
wutongyuonce <3304984553@qq.com>
2024-12-14 11:45:06 +0800

    1

--- homework/homework/homework/homework.cpp
+++ homework/homework/homework/homework.cpp
@@ -1,21 +1,42 @@
 ﻿#include <stdio.h>
 
 int main() {
-	// 数字1、2、3、4
-	int digits[] = { 1, 2, 3, 4 };
+    // 定义一个二维数组并初始化
+    int arr[4][5] = {
+        {12, 20, 30, 25, 52},
+        {36, 65, 22, 26, 60},
+        {70, 54, 12, 14, 6},
+        {2, 7, 8, 12, 15}
+    };
 
-	// 使用三重循环生成所有三位数
-	for (int i = 0; i < 4; i++) { // 百位
-		for (int j = 0; j < 4; j++) { // 十位
-			for (int k = 0; k < 4; k++) { // 个位
-				// 保证百位、十位、个位的数字不重复
-				if (i != j && j != k && i != k) {
-					// 输出符合条件的三位数
-					printf("%d%d%d\n", digits[i], digits[j], digits[k]);
-				}
-			}
-		}
-	}
+    // 存储每列平均值的一维数组
+    float averages[5];
 
-	return 0;
+    // 计算每列的平均值
+    for (int col = 0; col < 5; col++) {
+        int sum = 0;
+        // 计算当前列所有行的和
+        for (int row = 0; row < 4; row++) {
+            sum += arr[row][col];
+        }
+        // 计算平均值
+        averages[col] = (float)sum / 4;
+    }
+
+    // 输出二维数组各元素的值
+    printf("二维数组的元素值（按行列格式）：\n");
+    for (int row = 0; row < 4; row++) {
+        for (int col = 0; col < 5; col++) {
+            printf("%d ", arr[row][col]);
+        }
+        printf("\n");
+    }
+
+    // 输出一维数组各列的平均值
+    printf("\n各列的平均值：\n");
+    for (int col = 0; col < 5; col++) {
+        printf("第 %d 列的平均值: %.2f\n", col + 1, averages[col]);
+    }
+
+    return 0;
 }
